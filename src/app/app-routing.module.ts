b63ed2546282f11f658b7a237172b81e31c6ecdb
julianza32 <julianza32@gmail.com>
2022-04-03 14:12:14 -0500

    tercer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard', component: DashboardComponent,
+    children: [
+      {
+        path: "",
+        loadChildren: () => import('./components/y.module').then(mod => mod.YModule)
+      }
+    ]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'login' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
